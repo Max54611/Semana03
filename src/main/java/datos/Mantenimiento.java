e7fd43dabc30274c98e9ec5eef0e9572753bfb5c
Max54611 <magno.mori@tecsup.edu.pe>
2023-04-14 16:35:36 -0500

    2

--- src/main/java/datos/Mantenimiento.java
+++ src/main/java/datos/Mantenimiento.java
@@ -5,9 +5,10 @@ import javax.swing.JOptionPane;
 public class Mantenimiento {
 	public Connection conn;
 	public Statement sen;
+	public PreparedStatement sen2;
 	public ResultSet data;
 	public String driver="com.mysql.cj.jdbc.Driver";
-	public String cadena="jdbc:mysql://localhost/base";
+	public String cadena="jdbc:mysql://localhost/VENTAS2023";
 	public String usuario="root";
 	public String clave="";
 	public Connection conectar() {
@@ -21,5 +22,28 @@ public class Mantenimiento {
 		}
 		return conn;	
 	}
-
+	public ResultSet consulta(String sql) {
+		try {
+			sen=conectar().createStatement();
+			data=sen.executeQuery(sql);
+		}catch(SQLException e3) {
+			JOptionPane.showMessageDialog(null, "Error en la consulta");
+		}
+		return data;
+	}
+	
+	public boolean insertar(int codigo, String nombre, int cantidad, double precio) {
+		try {
+			String sql="insert into producto values(?,?,?,?)";
+			sen2=conectar().prepareStatement(sql);
+			sen2.setInt(1, codigo);
+			sen2.setString(2, nombre);
+			sen2.setInt(3, cantidad);
+			sen2.setDouble(4, precio);
+			sen2.executeUpdate();
+		}catch (SQLException e3) {
+			JOptionPane.showMessageDialog(null,"error en la inserción");
+		}
+		return true;
+	}
 }
